9cf10b5164ea6b080968c3b72084d206d55d9294
JoeyLi666 <591670166@qq.com>
2023-01-16 16:14:21 +0800

    feat(itheima_spring_jdbc): add codes into git

---
+++ itheima_spring_jdbc/src/test/java/com/itheima/domain/Account.java
@@ -0,0 +1,33 @@
+package com.itheima.domain;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class Account {
+
+    private String name;
+    private double money;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "name='" + name + '\'' +
+                ", money=" + money +
+                '}';
+    }
+}
